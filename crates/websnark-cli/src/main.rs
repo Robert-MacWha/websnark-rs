2329ef8ce38235cba6c6633ff91b584f6c98a7ea
Robert MacWha <github@macwha.com>
2026-06-02 17:14:45 -0400

    refactor: move websnark-rs into crates dir

---
+++ crates/websnark-cli/src/main.rs
@@ -0,0 +1,36 @@
+// use clap::{Parser, SubCommand};
+
+// struct Cli {
+//     #[clap(subcommand)]
+//     command: Commands,
+// }
+
+// #[derive(SubCommand)]
+// enum Commands {
+//     ConvertCircuit {
+//         #[arg(short, long)]
+//         input: String,
+//         #[arg(short, long)]
+//         output: String,
+//     },
+//     ConvertProvingKey {
+//         #[arg(short, long)]
+//         input: String,
+//         #[arg(short, long)]
+//         output: String,
+//     },
+//     ConvertVerificationKey {
+//         #[arg(short, long)]
+//         input: String,
+//         #[arg(short, long)]
+//         output: String,
+//     },
+// }
+
+// fn main() {
+//     let cli = Cli::parse();
+
+//     todo!()
+// }
+
+fn main() {}
